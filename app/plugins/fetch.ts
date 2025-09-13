01f7ad7b954aeb94e5be95557864b9d9fbbbfbd7
serii <seriiburduja@gmail.com>
2025-09-13 13:36:10 +0300

    feat: create post with auth need to finish

--- app/plugins/fetch.ts
+++ app/plugins/fetch.ts
@@ -2,11 +2,15 @@ export default defineNuxtPlugin({
   name: "fetch",
   parallel: true,
   setup(nuxtApp) {
+    const auth_store = useAuthStore();
+    const token = auth_store.token;
+
     const api = $fetch.create({
       baseURL: "http://localhost:3001",
       onRequest({ request, options, error }) {
         options.headers.set("Content-type", "application/json");
         options.headers.set("Accept", "application/json");
+        options.headers.set("Authorization", "Bearer " + token);
         // options.headers.set("Authorization", "Bearer " + "mynewtoken");
       },
       async onResponseError({ response }) {
