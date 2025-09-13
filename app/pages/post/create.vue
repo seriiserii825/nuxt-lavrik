583c72b916bc7614763e3d25b19759f46ff2930b
serii <seriiburduja@gmail.com>
2025-09-13 13:02:33 +0300

    feat: start page create post

---
+++ app/pages/post/create.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="post-create flex-[600px] flex items-center justify-center">
+    <h2 class="text-2xl font-bold text-center text-indigo-600 mb-6">Create Post</h2>
+  </div>
+</template>
--- app/pages/post/index.vue
+++ app/pages/post/index.vue
@@ -1,13 +1,19 @@
 <script setup lang="ts">
 import type { TPost } from "~/types/TPost";
 
-const posts = ref<TPost[]>([]);
 const { data, pending, error } = await useAPI<TPost[]>("/posts");
 </script>
 
 <template>
   <!-- Posts List -->
-  <main class="container mx-auto px-6 py-12">
+  <main class="container mx-auto px-6 py-6">
+    <header class="mb-8">
+      <NuxtLink
+        to="/post/create"
+        class="bg-indigo-600 text-white px-6 py-3 rounded-md hover:bg-indigo-700">
+        Create New Post
+      </NuxtLink>
+    </header>
     <h2 v-if="pending">Loading: {{ pending }}</h2>
     <div v-else-if="error" class="text-red-600">Error: {{ error.message }}</div>
     <div v-else>
